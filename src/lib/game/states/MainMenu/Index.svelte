991c5c4c3043585a4b4b8447bc4b5e6cbd53ea34
Nick Troshnev <nik.troshnev@gmail.com>
2023-05-13 05:56:17 +0200

    Add transition

--- src/lib/game/states/MainMenu/Index.svelte
+++ src/lib/game/states/MainMenu/Index.svelte
@@ -3,22 +3,25 @@
 	import Dictionaries from '$lib/game/states/MainMenu/Dictionaries.svelte'
 	import Settings from '$lib/game/states/MainMenu/Settings.svelte'
 	import { Tabs, Tab, TabLink } from '$lib/components/tabs'
+	import { fade } from 'svelte/transition'
 </script>
 
-<Tabs class="flex flex-col items-center">
-	<nav slot="headers" class="py-6 mx-auto w-full sm:w-[400px] flex gap-2">
-		<TabLink class="w-full" tabId="play" header="Играть" active />
-		<TabLink class="w-full" tabId="dictionaryOptions" header="Словари" />
-		<TabLink class="w-full" tabId="settings" header="Настройки" />
-	</nav>
+<div transition:fade={{ duration: 600 }}>
+	<Tabs class="flex flex-col items-center">
+		<nav slot="headers" class="py-6 mx-auto w-full sm:w-[400px] flex gap-2">
+			<TabLink class="w-full" tabId="play" header="Играть" active />
+			<TabLink class="w-full" tabId="dictionaryOptions" header="Словари" />
+			<TabLink class="w-full" tabId="settings" header="Настройки" />
+		</nav>
 
-	<Tab class="w-full sm:w-[400px]" tabId="play">
-		<Play />
-	</Tab>
-	<Tab class="w-full" tabId="dictionaryOptions">
-		<Dictionaries />
-	</Tab>
-	<Tab class="w-full sm:w-[400px]" tabId="settings">
-		<Settings />
-	</Tab>
-</Tabs>
+		<Tab class="w-full sm:w-[400px]" tabId="play">
+			<Play />
+		</Tab>
+		<Tab class="w-full" tabId="dictionaryOptions">
+			<Dictionaries />
+		</Tab>
+		<Tab class="w-full sm:w-[400px]" tabId="settings">
+			<Settings />
+		</Tab>
+	</Tabs>
+</div>
